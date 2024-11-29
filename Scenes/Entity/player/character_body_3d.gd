ce39eb5ba9e6d62115fa15bc20927bcadb74a855
Zilayer <91627169+Zilayer@users.noreply.github.com>
2024-11-29 20:01:47 +0500

    Add files via upload

--- Scenes/Entity/player/character_body_3d.gd
+++ Scenes/Entity/player/character_body_3d.gd
@@ -1,9 +1,11 @@
 extends CharacterBody3D
 
-
+const ROTATION = 0.01
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
+var rot_x = 0
+var rot_y = 0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -16,7 +18,7 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir := Input.get_vector( "ui_right", "ui_left", "ui_up", "ui_down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
@@ -26,3 +28,14 @@ func _physics_process(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		rot_y -= event.relative.x * ROTATION
+		rot_x -= event.relative.y * ROTATION
+		
+		if rot_x > 1.3: rot_x = 1.3
+		if rot_x < -1.3: rot_x = -1.3
+		
+		transform.basis = Basis(Vector3(0,1,0), rot_y)
+		$Camera3D.transform.basis = Basis(Vector3(1,0,0), rot_x)
